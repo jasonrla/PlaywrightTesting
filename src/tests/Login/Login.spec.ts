6a31023f49d3073f0e96c51b86d6210d3e1a7ffe
Jason Lopez <jason.lopez@tribal.credit>
2023-10-06 17:30:24 -0500

    update code

--- src/tests/Login/Login.spec.ts
+++ src/tests/Login/Login.spec.ts
@@ -5,31 +5,64 @@ import { Login } from '../../pages/Login';
 import { Dashboard } from '../../pages/Dashboard';
 import { Transactions } from '../../pages/Payments/Transactions';
 import { SendPayment } from '../../pages/Payments/SendPayment';
+import fs from 'fs';
+import path from 'path';
+import yaml from 'js-yaml';
+
+let env: any;
+let url;
+
+interface EnvironmentConfig {
+  url: string;
+  email: string;
+  password: string;
+}
+
+interface ConfigStructure {
+  PROD: EnvironmentConfig;
+  DEV: {
+    MX: EnvironmentConfig;
+  };
+}
 
 test.beforeEach(async ({ page }) => {
   await page.setViewportSize({ width: 1440, height: 900 });
   test.setTimeout(60000);
+  env = process.env.ENV || 'DEV';
+  url = await getURL(env);
+  await page.goto(url);
 });
 
 test('Testing Tribal Login has title', async ({ page }) => {
-  const env = process.env.ENV || 'DEV';
-  const url = await getURL(env);
-  console.log(url);
-  await page.goto(url);
   await expect(page).toHaveTitle(/Tribal/);
 });
 
 test('Testing login with no credentials', async ({ page }) => {
-  const env = process.env.ENV || 'DEV';
-  const url = await getURL(env);
-  await page.goto(url);
-  console.log(url);
   await page.getByRole('button', { name: 'Sign in' }).click();
   await expect(page.getByText('Please enter your email')).toBeVisible();
   await expect(page.getByText('Please enter a password')).toBeVisible();
 });
 
-test('Testing Login in'+ process.env.ENV, async ({ page }) => {
-const env = process.env.ENV || 'DEV';
-await login(page, env);
+test('Testing Login in '+ env, async ({ page }) => {
+  const configPath = path.resolve('src/config.yml');
+  const configContent = fs.readFileSync(configPath, 'utf8');
+  const config = yaml.load(configContent) as ConfigStructure;
+
+  let email: string;
+  let password: string;
+
+  if (env === "PROD") {
+    email = config.PROD.email;
+    password = config.PROD.password;
+  } else if (env === "DEV") {
+    email = config.DEV.MX.email;
+    password = config.DEV.MX.password;
+  } else {
+    throw new Error(`Invalid environment: ${env}`);
+  }
+
+  await page.getByPlaceholder('Email').fill(email);
+  await page.getByPlaceholder('Password').fill(password);
+  await page.getByRole('button', { name: 'Sign in' }).click();
+
 });
