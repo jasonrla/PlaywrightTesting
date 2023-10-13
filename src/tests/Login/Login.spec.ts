000cca444930cc3bc35ecc5b4a9ab0f91722ac30
Jason Lopez <jason.lopez@tribal.credit>
2023-10-13 16:06:48 -0500

    test chromium + mozilla + safari

--- src/tests/Login/Login.spec.ts
+++ src/tests/Login/Login.spec.ts
@@ -3,16 +3,9 @@ import { login } from '../../utils/loginHelper';
 import { Login } from '../../pages/Login';
 
 test.beforeEach(async ({ page }) => {
-  //await page.setViewportSize({ width: 1440, height: 900 });
+  await page.setViewportSize({ width: 1440, height: 900 });
 });
-/*
-test('has title', async ({ page }) => {
-  await page.goto('https://dev.app.tribalcredit.io/login', { timeout: 10000 });
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Tribal/);
-});
-*/
 test('Sign in with no credentials', async ({ page }) => {
   await page.goto('https://dev.app.tribalcredit.io/login');
 
